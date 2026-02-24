089464b00767e3f8278e7799608b7f259b2b79e8
Sefa İMAMOĞLU <mehmetsefaimamoglu@gmail.com>
2026-02-24 14:23:17 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Sefa İMAMOĞLU</p>
+      <p>Ogrenci No: 230541038</p>
+      <p>Bolum: YAZILIM MÜHENDİSLİĞİ</p>
+      <p>Hobiler: Futbol,Yüzme,Kitap Okumak,Film İzlemek,Müzik Dinlemek</p>
+    </div>
+  )
+}
+
+export default App
+
